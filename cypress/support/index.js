827518dbe52cee18f19038a0f9477b596ec247e4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-14 09:10:03 -0400

    add comments

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -3,6 +3,9 @@ require('cypress-react-unit-test/support')
 
 // custom command to make taking snapshots with full name
 // formed from the test title + suffix easier
+// cy.visualSnapshot() // default full test title
+// cy.visualSnapshot('clicked') // full test title + ' - clicked'
+// also sets the width and height to the current viewport
 Cypress.Commands.add('visualSnapshot', (maybeName) => {
   let snapshotTitle = cy.state('runnable').fullTitle()
   if (maybeName) {
