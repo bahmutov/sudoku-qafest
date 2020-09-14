9f17cb87946a491cc96784a5e136228f5441a460
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-14 08:57:50 -0400

    update App spec and enable visual tests (#7)

--- src/App.spec.js
+++ src/App.spec.js
@@ -2,69 +2,14 @@
 import React from 'react'
 import { App } from './App'
 import { mount } from 'cypress-react-unit-test'
-import { getSudoku } from './solver/sudoku'
 import * as UniqueSudoku from './solver/UniqueSudoku'
 import initArray from '../cypress/fixtures/init-array.json'
 import solvedArray from '../cypress/fixtures/solved-array.json'
 
 describe('App', () => {
-  it('shows the timer', () => {
-    cy.clock()
-    mount(<App />)
-    cy.contains('.status__time', '00:00') // .percySnapshot('timer-zero')
-    cy.tick(700 * 1000)
-    cy.contains('.status__time', '11:40') // .percySnapshot('timer-passed')
-  })
-
-  it('checks the entire game', () => {
-    cy.clock()
-    mount(<App />)
-    cy.get('.game__cell--filled').should('have.length', 45)
-    cy.get('.game__cell').each(($cell) => $cell.css('opacity', '0'))
-    // cy.get('.container').percySnapshot('game-container')
-  })
-
-  it('shows deterministic board', () => {
-    const str =
-      '713.94528294851637568...914871935246425186379936472185.8..4.7...57..849..4....8..'
-    const sudoku = getSudoku()
-    cy.stub(sudoku, 'generate').returns(str)
-    cy.stub(Math, 'random').returns(0.5)
-
-    cy.clock()
-    mount(<App />)
-    cy.get('.game__cell--filled').should('have.length', 45)
-    // cy.get('.container').percySnapshot('same-game-container')
-
-    // cy.viewport('iphone-6')
-    // cy.get('.container').percySnapshot('same-game-container-iphone6', {
-    //   widths: [400],
-    // })
-
-    // cy.viewport(250, 400)
-    // cy.get('.container').percySnapshot('same-game-container-250px', {
-    //   widths: [250],
-    // })
-  })
-
-  it('plays a move', () => {
-    const str =
-      '713.94528294851637568...914871935246425186379936472185.8..4.7...57..849..4....8..'
-    const sudoku = getSudoku()
-    cy.stub(sudoku, 'generate').returns(str)
-    cy.stub(Math, 'random').returns(0.5)
-
-    cy.clock()
-    mount(<App />)
-    cy.get('.game__cell').first().click()
-    cy.contains('.status__number', '7').click()
-    cy.get('.game__cell')
-      .first()
-      .should('have.class', 'game__cell--highlightselected')
-    // cy.get('.container').percySnapshot('same-game-container-move')
-  })
-
   it('mocks board creation', () => {
+    // load JSON files using cy.fixture calls
+    // https://on.cypress.io/fixture
     cy.fixture('init-array').then((initArray) => {
       cy.fixture('solved-array').then((solvedArray) => {
         cy.stub(UniqueSudoku, 'getUniqueSudoku').returns([
@@ -76,10 +21,12 @@ describe('App', () => {
     cy.clock()
     mount(<App />)
     cy.get('.game__cell--filled').should('have.length', 45)
-    // cy.get('.container').percySnapshot('same-game-mocked-sudoku')
+    // the visual snapshot will be the same
+    cy.visualSnapshot()
   })
 
   it('plays one move', () => {
+    // stub method import using JSON objects
     cy.stub(UniqueSudoku, 'getUniqueSudoku').returns([initArray, solvedArray])
     cy.clock()
     mount(<App />)
@@ -88,7 +35,7 @@ describe('App', () => {
     cy.get('.game__cell')
       .first()
       .should('have.class', 'game__cell--highlightselected')
-    // cy.get('.container').percySnapshot('same-game-made-one-move')
+    cy.visualSnapshot()
   })
 
   it('plays to win', () => {
@@ -101,44 +48,21 @@ describe('App', () => {
       .as('getUniqueSudoku')
     cy.clock()
     mount(<App />)
+    cy.visualSnapshot('1 game is almost solved')
+
+    // win the game
     cy.get('.game__cell').first().click()
-    // we can even look at the solved array!
+    // use the known number to fill the first cell
     cy.contains('.status__number', solvedArray[0]).click()
+
     // winning message displayed
     cy.get('.overlay__text').should('be.visible')
-    // cy.get('.container').percySnapshot('game-solved')
+    cy.visualSnapshot('2 game is solved')
 
     // clicking the overlay starts the new game
     cy.get('@getUniqueSudoku').should('have.been.calledOnce')
     cy.get('.overlay__text').click()
     cy.get('.overlay').should('not.be.visible')
     cy.get('@getUniqueSudoku').should('have.been.calledTwice')
-  })
-
-  it('restores the clock', () => {
-    // mock the clock with current timestamp
-    // so that when we restore it the elapsed time
-    // makes sense
-    cy.clock(+new Date())
-    mount(<App />)
-    // make sure the application has rendered
-    // and the synthetic clock started working
-    cy.get('.game__cell--filled').should('have.length', 45)
-
-    cy.tick(600 * 1000) // 10 minutes
-    cy.contains('.status__time', '10:00')
-
-    // resume the clock
-    // cy.tick().then(clock => {
-    //   clock.restore()
-    // })
-
-    // shortcut way to call "clock.restore("
-    cy.tick().invoke('restore')
-
-    // the clock is restored to original value
-    // thus the timer will start measuring again
-    // from the original date passed to "cy.clock"
-    cy.contains('.status__time', '00:03')
   })
 })
