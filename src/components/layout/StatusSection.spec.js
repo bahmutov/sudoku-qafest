5f55c8787057cbd9a0f0c623667da9527826502d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-18 13:57:09 -0400

    enable status visual tests (#9)

--- src/components/layout/StatusSection.spec.js
+++ src/components/layout/StatusSection.spec.js
@@ -18,8 +18,7 @@ describe('StatusSection', () => {
       .find('input[type=checkbox]')
       .should('not.be.checked')
 
-    // if you want to use full test title + suffix
-    // cy.percySnapshot(cy.state('runnable').fullTitle() + ' - status')
+    cy.visualSnapshot('1 - modes off')
 
     cy.log('**turn both modes on**')
     cy.get('.status__action-mistakes-mode')
@@ -34,6 +33,6 @@ describe('StatusSection', () => {
       .find('input[type=checkbox]')
       .should('be.checked')
 
-    // cy.percySnapshot(cy.state('runnable').fullTitle() + ' - status-both-modes')
+    cy.visualSnapshot('2- both modes')
   })
 })
